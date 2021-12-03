3f023c2785fd6c2a7e4f522c241ebb706fcec506
fareselsokary <fareselsokary11@gmail.com>
2021-12-03 18:15:15 +0200

    Initial Commit

---
+++ IOS Task/ViewControllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOS Task
+//
+//  Created by fares on 03/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
