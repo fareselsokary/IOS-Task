004336805a9e056465ba1c802d65b397292b76de
fareselsokary <fareselsokary11@gmail.com>
2021-12-04 17:32:04 +0200

    add UIStoryboard extension

---
+++ IOS Task/Helper/Extensions/UIStoryboard.swift
@@ -0,0 +1,15 @@
+//
+//  UIStoryboard.swift
+//  IOS Task
+//
+//  Created by fares on 04/12/2021.
+//
+
+import UIKit
+
+extension UIStoryboard {
+    static var Main: UIStoryboard {
+        return UIStoryboard(name: "Main", bundle: nil)
+    }
+}
+
