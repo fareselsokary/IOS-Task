6782cb4c32c569a8f411bf4bc8147b717012d46b
fareselsokary <fareselsokary11@gmail.com>
2021-12-04 17:32:32 +0200

    add Document model

--- IOS Task/Model/BaseResponse.swift
+++ IOS Task/Model/BaseResponse.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+// MARK: - BaseResponse
+
+class BaseResponse: Codable {
+    var numFound, start: Int?
+    var numFoundExact: Bool?
+    var docs: [Document]?
+
+    init(numFound: Int?, start: Int?, numFoundExact: Bool?, docs: [Document]?, q: String?) {
+        self.numFound = numFound
+        self.start = start
+        self.numFoundExact = numFoundExact
+        self.docs = docs
+    }
+}
