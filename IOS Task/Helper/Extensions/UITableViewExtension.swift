2318a8481eb61b707daa03081998516be15af520
fareselsokary <fareselsokary11@gmail.com>
2021-12-05 10:44:56 +0200

    add ui table view extension

--- IOS Task/Helper/Extensions/UITableViewExtension.swift
+++ IOS Task/Helper/Extensions/UITableViewExtension.swift
@@ -5,4 +5,31 @@
 //  Created by fares on 04/12/2021.
 //
 
-import Foundation
+import UIKit
+
+extension UITableView {
+    func setEmptyMessage(_ message: String) {
+        let messageLabel = UILabel(frame: CGRect(x: 0, y: 0, width: bounds.size.width, height: bounds.size.height))
+        messageLabel.text = message
+        messageLabel.textColor = .lightGray
+        messageLabel.numberOfLines = 0
+        messageLabel.textAlignment = .center
+        messageLabel.font = .boldSystemFont(ofSize: 15)
+        messageLabel.sizeToFit()
+        backgroundView = messageLabel
+    }
+
+    func restore() {
+        backgroundView = nil
+    }
+}
+
+extension UICollectionView {
+    func centerItemsInCollectionView(cellWidth: Double, numberOfItems: Double, spaceBetweenCell: Double) -> UIEdgeInsets {
+        let totalWidth = cellWidth * numberOfItems
+        let totalSpacingWidth = spaceBetweenCell * (numberOfItems - 1)
+        let leftInset = (frame.width - CGFloat(totalWidth + totalSpacingWidth)) / 2
+        let rightInset = leftInset
+        return UIEdgeInsets(top: 0, left: leftInset, bottom: 0, right: rightInset)
+    }
+}
