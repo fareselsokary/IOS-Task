7fc66fb0372a953ee15011ab947c5a44adca20a5
fareselsokary <fareselsokary11@gmail.com>
2021-12-05 10:44:49 +0200

    add ui text field extension

--- IOS Task/Helper/Extensions/UITextFieldExtension.swift
+++ IOS Task/Helper/Extensions/UITextFieldExtension.swift
@@ -5,4 +5,16 @@
 //  Created by fares on 05/12/2021.
 //
 
-import Foundation
+import UIKit
+import Combine
+
+extension UITextField {
+    var textPublisher: AnyPublisher<String, Never> {
+        NotificationCenter.default
+            .publisher(for: UITextField.textDidChangeNotification, object: self)
+            .compactMap { $0.object as? UITextField } // receiving notifications with objects which are instances of UITextFields
+            .compactMap(\.text) // extracting text and removing optional values (even though the text cannot be nil)
+            .eraseToAnyPublisher()
+    }
+}
+
