4d783171b54e424822213c1f09849a441e2be18b
fareselsokary <fareselsokary11@gmail.com>
2021-12-09 15:38:22 +0200

    Revert "use combine with urlSession to load image"
    This reverts commit 37065ca16d3170773381175861fbc2abd562f6fe.

--- IOS Task/Helper/Extensions/UIImageViewExtension.swift
+++ IOS Task/Helper/Extensions/UIImageViewExtension.swift
@@ -5,33 +5,25 @@
 //  Created by fares on 05/12/2021.
 //
 
-import Combine
 import UIKit
 
-private var subscriptions: Set<AnyCancellable> = []
-
 extension UIImageView {
-    private func downloaded(from url: URL) -> AnyPublisher<UIImage?, Error> {
-        return URLSession.shared.dataTaskPublisher(for: url)
-            .tryMap { response in
-                UIImage(data: response.data)
+    func downloaded(from url: URL) {
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard
+                let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
+                let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
+                let data = data, error == nil,
+                let image = UIImage(data: data)
+            else { return }
+            DispatchQueue.main.async { [weak self] in
+                self?.image = image
             }
-            .receive(on: DispatchQueue.main)
-            .eraseToAnyPublisher()
+        }.resume()
     }
 
     func downloaded(from link: String) {
         guard let url = URL(string: link) else { return }
-        downloaded(from: url).sink { completion in
-            switch completion {
-            case .finished:
-                print("fininsh download image")
-            case let .failure(error):
-                print(error)
-            }
-        } receiveValue: { [weak self] image in
-            self?.image = image
-        }
-        .store(in: &subscriptions)
+        downloaded(from: url)
     }
 }
